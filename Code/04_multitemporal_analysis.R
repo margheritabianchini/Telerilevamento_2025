e9d230b554e71267d20d5207a5a1c6603cf60edb
margheritabianchini <margherita.bianchini@hotmail.it>
2025-03-18 17:25:44 +0100

    Create 04_multitemporal_analysis.R

---
+++ Code/04_multitemporal_analysis.R
@@ -0,0 +1,99 @@
+#R code for performing multitemporal analysis 
+#è analizzata la distribuzione multitemporale dell'NO3 nel tempo
+
+library(imageRy)
+library(terra)
+library(viridis)
+
+im.list()
+
+EN_01=im.import("EN_01.png")
+EN_01=flip(EN_01)
+plot(EN_01)
+#immagine gennaio 2020
+
+#è un'immagine RGB in raimbow colors (i colori in questo caso non vanno bene)
+#zone in cui il traffico veicolare è maggiore hanno valori più alti 
+
+EN_13=im.import("EN_13.png")
+EN_13=flip(EN_13)
+plot(EN_13)
+#nell'immagine di marzo 2020
+#si vede che gli spot sono molto diminuiti
+
+#Exercise: plot the two images one beside the other
+im.multiframe(1,2)
+plot(EN_01)
+plot(EN_13)
+
+
+#differenza tra le due immagini (gennaio-marzo)
+ENdif = EN_01[[1]] - EN_13[[1]] 
+plot(ENdif)
+
+#differenza (giallo) -> spostati verso gennaio
+#differenza inversa (blu) -> spostati verso marzo -> aree in cui sono rimasti alti i valori o addirittura maggiori 
+
+#plot con colore inferno di viridis
+plot(ENdif, col=inferno(100))
+
+#--------
+
+
+
+#Greenland dataset ice melt
+#in base alla temperatura 
+#le temperature più basse si concentrano al centro 
+
+gr=im.import("greenland")
+im.multiframe(1,2) 
+plot(gr[[1]], col=rocket(100))
+plot(gr[[4]], col=rocket(100))
+
+#in questo caso si possono individuare anche delle fluttuazioni e non un cambiamento climatico reale
+
+grdif = gr[[1]] - gr[[4]] 
+dev.off()
+plot(grdif)
+
+#la parte blu sono più alti valori di temperatura nel 2015
+
+#Exporting data 
+#selezionare la cartella di lavoro
+setwd("C://Users/margh/Desktop/GEOLOGIA E TERRITORIO (LM-74)/TELERILEVAMENTO")
+
+getwd()
+
+plot(gr)
+
+#con png creiamo un file png 
+png("greenland_output.png")
+plot(gr) 
+dev.off()
+#con dev.off chiudiamo tutto 
+
+#oltre al png si possono fare altri formati di file 
+pdf("greenland_output.pdf")
+plot(gr) 
+dev.off()
+
+
+pdf("difgreen.pdf")
+plot(grdif)
+dev.off()
+
+#nel pdf non ho problemi di risoluzione dei numeri perchè è un vettoriale
+jpeg("difgreen.jpeg")
+plot(grdif)
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
