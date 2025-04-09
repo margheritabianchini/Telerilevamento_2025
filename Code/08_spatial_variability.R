9ec642818f075b81b08fa07e2d5aae7e52abb51f
margheritabianchini <margherita.bianchini@hotmail.it>
2025-04-09 17:45:35 +0200

    Update 08_spatial_variability.R

--- Code/08_spatial_variability.R
+++ Code/08_spatial_variability.R
@@ -3,6 +3,10 @@
 
 library(terra)
 library(imageRy)
+library(viridis)
+library(patchwork)
+install.packages("RStoolbox")
+library(RStoolbox)
 
 # 24 26 25 
 # ci riferiamo al calcolo della deviazione standard sulla popolazione 
@@ -53,6 +57,60 @@ im.plotRGB(sent, r=3, g=2, b=1)
 # verde fluorescente sono le praterie in alta quota 
 # il bianco è neve
 # oppure possono essere delle nuvole 
+
+
+
+#-----------------
+
+nir = sent[[1]]
+
+# Exercise: plot the NIR band with the inferno 
+
+plot(nir, col=inferno(100))
+
+sd3 = focal(nir, w=c(3,3), fun=sd)
+plot(sd3)
+# vediamo la deviazione standard su una matrice 3 x 3 
+# si vedono le zone in ciu c'è stata una variazione dell'infrarosso vicino
+# variazione in crepacci, vegetazione 
+
+im.multiframe(1,2)
+im.plotRGB(sent, r=1, g=2, b=3)
+plot(sd3)
+
+# calcoliamo la deviazione standard su una finestra 5x5 
+# per variazioni di dettaglio userò una moving window ristretta
+
+dev.off()
+sd5 = focal(nir, w=c(5,5), fun=sd)
+plot(sd5)
+
+
+im.multiframe(1,2)
+plot(sd3)
+plot(sd5)
+
+
+
+# Exercise: use ggplot to plot the standard deviation
+# funzione pensata per plottare delle singole bande 
+im.ggplot(sd3)
+
+# Exercise: plot the two sd maps (3 or 5) one beside the other with ggplot 
+p1 = im.ggplot(sd3)
+p2 = im.ggplot(sd5)
+
+p1 + p2
+
+
+# Plot the original nir and the standard deviations 
+p3=im.ggplot(nir)
+p3+p1
+
+
+
+
+
 
 
 
