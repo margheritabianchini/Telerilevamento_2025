1db3f28ac895741be5e4b48b4acf7f4fcd3e3c61
margheritabianchini <margherita.bianchini@hotmail.it>
2025-04-10 15:50:21 +0200

    Create 09_packages_from_GitHub.R

---
+++ Code/09_packages_from_GitHub.R
@@ -0,0 +1,21 @@
+# R codes for installing packages from GitHub
+
+# From GitHub:
+library(devtools) #or remotes
+install_github("ducciorocchini/cblindplot")
+library(cblindplot)
+
+# From GitHub:
+# install.packages("dev.tools")
+install_github("clauswilke/colorblindr")
+# i pacchetti sul cran vanno revisionati
+# mentre i pacchetti su github restano per sempre
+library(colorblindr)
+
+# From CRAN: 
+install.packages("colorblindcheck")
+library(colorblindcheck)
+
+
+
+
