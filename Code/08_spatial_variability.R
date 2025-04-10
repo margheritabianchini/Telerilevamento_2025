421bc333e516a27f410d744f9e51d975693be243
margheritabianchini <margherita.bianchini@hotmail.it>
2025-04-10 15:06:20 +0200

    Update 08_spatial_variability.R

--- Code/08_spatial_variability.R
+++ Code/08_spatial_variability.R
@@ -108,9 +108,80 @@ p3=im.ggplot(nir)
 p3+p1
 
 
+# What to do in case of huge images 
+# RICAMPIONAMENTO - riduzione della risoluzione spaziale 
 
+im.list()
+sent = im.import("sentinel.png")
+sent = flip(sent)
+plot(sent)
 
+# numero di pixel totali
+ncell(sent) * nlyr(sent)
+# 794 * 798
+# 2534448
 
+# ricampionamento
+senta = aggregate(sent, fact=2)
+# la risoluzione passa 2 * 2 perchè moltiplico per 2 ogni lato del quadrato che rappresenta il pixel 
+# il pixel iniziale è 1/4 di quello in uscita 
+ncell(senta) * nlyr(senta)
+# dividendoli otteniamo 4 
+# perchè lo abbiamo ridotto di 4 volte 
+
+senta5 = aggregate(sent, fact=5)
+ncell(senta5) * nlyr(senta5)
+
+# nel momento in cui si ricampiona significa ridurre del quadrato
+
+#CALCOLO DEVIAZIONE STANDARD 
+nira = senta [[1]]
+sd3a = focal(nira, w=c(3,3), fun=sd)
+
+# Exercise: make a multiframe and plot in RGB the three images (or, 2, 5)
+im.multiframe(1,3)
+im.plotRGB(sent, r=1, g=2, b=3)
+im.plotRGB(senta, r=1, g=2, b=3)
+im.plotRGB(senta5, r=1, g=2, b=3)
+
+# si vede che con fattore 5 si perde il dettaglio dell'immagine 
+# non sempre la risoluzione max è meglio di quella più bassa 
+# es. immagine ad alta risoluzione può creare più rumore
+
+nira = senta [[1]]
+sd3a = focal(nira, w=c(3,3), fun=sd)
+plot(sd3a)
+
+# Exercise: calculate the standard deviation for the factor 5 image 
+nira5 = senta5 [[1]]
+sd3a5 = focal(nira5, w=c(3,3), fun=sd)
+plot(sd3a5)
+
+sd5a5 = focal(nira5, w=c(5,5), fun=sd)
+plot(sd5a5)
+
+im.multiframe(1,2)
+plot(sd5a5)
+plot(sd3a)
+
+im.multiframe(2,2)
+plot(sd3)
+plot(sd3a) # fattore ric 2
+plot(sd3a5) # fattore ric 5
+plot(sd5a5) # fattore ric 5 e finestra mobile 5*5
+
+p1 = im.ggplot(sd3)
+p2 = im.ggplot(sd3a)
+p3 = im.ggplot(sd3a5)
+p4 = im.ggplot(sd5a5)
+
+p1 + p2 + p3 + p4 
+
+im.multiframe(2,2)
+plot(sd3, col=mako(100))
+plot(sd3a, col=mako(100)) 
+plot(sd3a5, col=mako(100)) 
+plot(sd5a5, col=mako(100)) 
 
 
 
