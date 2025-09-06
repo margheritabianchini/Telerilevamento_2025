9eaa8465318b357f5065b1cb4167c6daba143f2a
margheritabianchini <margherita.bianchini@hotmail.it>
2025-09-06 15:44:30 +0200

    Update 01_first_script.R

--- Code/01_first_script.R
+++ Code/01_first_script.R
@@ -1,28 +1,28 @@
 #FIRST R CODING FROM SCATCH 
 
-#operation 
+#Operation 
 2+3
 
 
-#object 
+#Object 
 anna <- 2 + 3     #assign an operation to an object 
 chiara <- 4 + 6 
 anna + chiara 
 
 
-#array 
+#Array 
 filippo <- c(0.2, 0.4, 0.6, 0.8, 0.9)     #creating an array
 luca <- c(100, 80, 60, 50, 10) 
 
 
-#plot
+#Plot
 plot (luca, filippo, pch=19)    #define a plot [pch=plotting character]
 plot (luca, filippo, pch=19, col="blue")     #[coL=color]
 plot (luca, filippo, pch=19, col="blue", cex=2)     #[cex=character esxpansions]
 plot (luca, filippo, pch=19, col="blue", cex=2, xlab="rubbish", ylab="biomass")     #[xlab=x-axis label name, ylab=y-axis label name]
 
 
-#installing packages 
+#Packages 
 #CRAN: it is a network of servers, which represents a repository of R resources
 install.packages("terra")     #installation of the package 
 library(terra)     #package loading 
