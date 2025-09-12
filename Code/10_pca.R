dfb3ce14f6f187d21ffd9f86e4860d35a8facaba
margheritabianchini <margherita.bianchini@hotmail.it>
2025-09-12 23:21:24 +0200

    Update 10_pca.R

--- Code/10_pca.R
+++ Code/10_pca.R
@@ -1,19 +1,25 @@
-# R code for Principal Component Analysis 
+#R CODE FOR PRINCIPAL COMPONENT ANALYSIS (PCA)
+#The principal component will be identified instead of choosing a band
+#the outputs do not change
 
-# cambia che invece di scegliere una banda verrà individuata la componente principale
-# gli output non cambiano
 
+#Packages 
 library(imageRy)
 library(terra)
 
+
+#Content of the package 
 im.list()
 
+
+#🛰️ SENTINEL-2 (spatial resolution = 10m)
 sent = im.import("sentinel.png")
 sent = flip(sent)
 plot(sent)
 
-# togliamo la quarta banda perchè non ci serve 
-# usiamo un subset 
+
+#we use a subset
+#the fourth band is removed because it is not needed
 
 
 sent = c(sent[[1]],sent[[2]],sent[[3]])
@@ -22,39 +28,36 @@ sent = c(sent[[1]],sent[[2]],sent[[3]])
 # green = band 3 
 
 
-
-sentpca = im.pca(sent, n_samples=100000)
-
-
-# a livello grafico la prima immagine ha molta più informazione 
-# infatti usare la terza banda non avrebbe senso 
-# quindi andiamo ad usare LA PRIMA COMPONENTE PRINCIPALE
+sentpca = im.pca(sent, n_samples=100000)    #[im.pca=calculates the principal components of pixel values]
+                                            #[n_samples=randomly samples a subset of pixels]
+                                            #graphically, the first image has much more information
+                                            #using the third band would not make sense
+                                            #so we use the first vertical component
 
 
 tot = 77 + 54 + 6 
 #137
 
+
 77*100 / tot
+#56.20438
 
 
+#Standard deviation
 sdpc1=focal(sentpca[[1]], w=c(3,3), fun="sd")
 plot(sdpc1)
 
-# questa volta abbiamo fatto un analisi multivariata
-# preso la componente con variabilità più alta 
-# e la abbiamo utilizzata per calcolare la standard deviation 
-# quindi rispetto la scorsa volta la scelta è stata oggettiva 
-# dato che c'è una correlazione. usare la pca è buona norma 
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#This is a multivariate analysis:
+#1. Take the component with the highest variability
+#2. Use it to calculate the standard deviation
+#So compared to last time the choice was objective
+#Since there is a correlation, using PCA is good practice
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-# quando si usano variabili
-# BISOGNA SEMPRE ANALIZZARLE SULLO STESSO RANGE 
-# Es. temperatura, elevation. sicuramente vincerà elevation
-# quindi bisogna standardizzarle. es. DIVIDENDO PER IL MAX 
-
-
-
-
-
-
-
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#When using variables, they must always be analyzed in the same range
+#Ex. temperature, elevation...the elevation will definitely be higher
+#So we need to standardize them ex. dividing by the maximum 
