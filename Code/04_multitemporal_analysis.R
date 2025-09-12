a966c7930245d8010c50cdec7de5ae5eef082b14
margheritabianchini <margherita.bianchini@hotmail.it>
2025-09-12 11:50:39 +0200

    Update 04_multitemporal_analysis.R

--- Code/04_multitemporal_analysis.R
+++ Code/04_multitemporal_analysis.R
@@ -1,126 +1,137 @@
-#R code for performing multitemporal analysis 
-#è analizzata la distribuzione multitemporale dell'NO3 nel tempo
+#R CODE FOR PERFORMING MULTITEMPORAL ANALYSIS
+#multitemporal distribution of NO3 over time
 
-install.packages("ggridges") #this is needed to create ridgeline plots 
+
+#Packages 
+install.packages("ggridges")     #this is needed to create ridgeline plots 
+library(ggridges)
 library(imageRy)
 library(terra)
 library(viridis)
-# library(ggridges)
 
 
-
-#Listing the data 
+#Content of the package 
 im.list()
 
+
+#JANUARY 2020 ----------------------------------------------------------------------------------------------------------------------------------------------------
 EN_01=im.import("EN_01.png")
 EN_01=flip(EN_01)
 plot(EN_01)
-#immagine gennaio 2020
+#RGB image with rainbow colors (wrong colors)
+#higher values -> areas where the vehicular traffic is higher 
 
-#è un'immagine RGB in raimbow colors (i colori in questo caso non vanno bene)
-#zone in cui il traffico veicolare è maggiore hanno valori più alti 
-
+#MARCH 2020 -------------------------------------------------------------------------------------------------------------------------------------------------------
 EN_13=im.import("EN_13.png")
 EN_13=flip(EN_13)
 plot(EN_13)
-#nell'immagine di marzo 2020
-#si vede che gli spot sono molto diminuiti
+#the spots have decreased
 
-#Exercise: plot the two images one beside the other
+
+#EXERCISE: plot the two images one beside the other
+#🖼️ Multiframe
 im.multiframe(1,2)
 plot(EN_01)
 plot(EN_13)
 
 
-#differenza tra le due immagini (gennaio-marzo)
-ENdif = EN_01[[1]] - EN_13[[1]] 
+#Difference between the two images --------------------------------------------------------------------------------------------------------------------------------
+ENdif = EN_01[[1]] - EN_13[[1]]     #January - March 
 plot(ENdif)
+#difference (yellow) -> move towards January -> areas where values ​ decreased 
+#inverse difference (blue) -> move towards March -> areas where values ​​remained high or even increased
 
-#differenza (giallo) -> spostati verso gennaio
-#differenza inversa (blu) -> spostati verso marzo -> aree in cui sono rimasti alti i valori o addirittura maggiori 
 
-#plot con colore inferno di viridis
+#Plot with inferno colors (from viridis package)
 plot(ENdif, col=inferno(100))
 
-#--------
 
-
-
-#Greenland dataset ice melt
-#in base alla temperatura 
-#le temperature più basse si concentrano al centro 
-
+#🧊 GREENLAND DATASET ICE MELT -----------------------------------------------------------------------------------------------------------------------------------
+#depending on the temperature
+#the lowest temperatures are concentrated in the center
 gr=im.import("greenland")
+
+
+#🖼️ Multiframe
 im.multiframe(1,2) 
-plot(gr[[1]], col=rocket(100))
-plot(gr[[4]], col=rocket(100))
+plot(gr[[1]], col=rocket(100))     #Greenland 2000
+plot(gr[[4]], col=rocket(100))     #Greenland 2015
+#in this case, fluctuations are also identified, not just actual climate change
 
-#in questo caso si possono individuare anche delle fluttuazioni e non un cambiamento climatico reale
 
-grdif = gr[[1]] - gr[[4]] 
+#Difference between the two images
+grdif = gr[[1]] - gr[[4]]     #Greenland 2000 - Greenland 2015
 dev.off()
-plot(grdif)
+plot(grdif)      #plot of the difference 
+#blue zones -> highest values of temperature in 2015
 
-#la parte blu sono più alti valori di temperatura nel 2015
 
-
-#RIDGELINE PLOTS: utile per analizare la distribuzione (es. frequenza)
-im.ridgeline(gr, scale=1)
-# in questo caso è difficile trovare un trend ma sono fluttuazioni
-# prende tutti i pixel e calcola le frequenze dei pixel
-
-# invece di rimanere nella propria parte di grafico va a sovrapporsi a quello successivo
+#📉 Ridgeline plots -----------------------------------------------------------------------------------------------------------------------------------------------
+#useful for analyzing the distribution (ex. frequency)
+im.ridgeline(gr, scale=1)     #[im.ridgeline=create a ridgeline plot]
+                              #[scale=set the vertical scale of the graph]
+                              #[palette=set the colors]
+#in this case it is difficult to find a trend because they are fluctuations
+#takes all the pixels and calculates the pixel frequencies
 
 im.ridgeline(gr, scale=2)
+#instead of remaining in its own part of the graph it overlaps the next one
 im.ridgeline(gr, scale=2, palette="inferno")
-#in questo modo cambia il colore del grafico 
-
-
 im.ridgeline(gr, scale=3, palette="inferno")
 
-#Exercise: import the NDVI data
+#⛰️ BRENTA MOUNTAIN RANGE ------------------------------------------------------------------------------------------------------------------------------------------
+#EXERCISE: import the NDVI data
 ndvi=im.import("Sentinel2_NDVI")
-#è il gruppo del brenta
-# dato che i nomi sono uguali lui è anadato a sovrascriverli
+
 
 # Changing names 
 # Sources:    Sentinel2_NDVI_2020-02-21.tif  
-              Sentinel2_NDVI_2020-05-21.tif  
-              Sentinel2_NDVI_2020-08-01.tif  
-              Sentinel2_NDVI_2020-11-27.tif  
-# come rinominare colonne di un dataframe 
-names(ndvi) = c("02_Feb", "05_May", "08_Aug", "11_Nov")
+#             Sentinel2_NDVI_2020-05-21.tif  
+#             Sentinel2_NDVI_2020-08-01.tif  
+#             Sentinel2_NDVI_2020-11-27.tif  
+
+
+# Change of the names
+names(ndvi) = c("02_Feb", "05_May", "08_Aug", "11_Nov") 
+
+
+#📉Ridgeline plot -----------------------------------------------------------------------------------------------------------------------------------------------
 im.ridgeline(ndvi, scale=2)
 
-# tutte le volte che c'è un problema grafico -> dev.off 
+#graphics problem -> devoff()
 
 plot(ndvi)
 im.ridgeline(ndvi, scale=2)
 im.ridgeline(ndvi, scale=2, palette="mako")
+#lower values -> lower biomass values ​​for the time of year
+#biomass values increase until August-September
+#biomass values return as before in November
 
-# avrà valori più bassi dato i valori inferiori di biomassa per il periodo dell'anno
-# aumentano fino a agosto_settembre
-# si torna alla situazione precedente a novembre
 
-
+#📈 Scatterplots -------------------------------------------------------------------------------------------------------------------------------------------------
 pairs(ndvi)
 plot(ndvi[[1]], ndvi[[2]])
-# y = x #may y, feb x 
-# y = a + bx 
-# a = 0, b = 1 
+
+# y = x 
+# May y-axis
+# February x-axis
+# y = a + bx (equation of a straight line)
+# a = 0, b = 1 (coefficients for equality x=y)
 # y = a + bx = 0 + 1x = x
 
-# linea 1 a 1 
-abline(0, 1, col="red")
+#1 to 1 line
+abline(0, 1, col="red")     #[abline=defines a straight line, specifying the coefficients] 
 
-# mettiamo i dataset con lo stesso range
+
+#Setting datasets with the same range
 plot(ndvi[[1]], ndvi[[2]], xlim=c(-0.3, 0.9), ylim=c(-0.3, 0.9))
-abline(0, 1, col="red")
+abline(0, 1, col="red")     #straight line
+#when the line is above, it means that the data is higher in May than in February
 
-# se sta sopra i dati sono superiori a maggio rispetto a febbraio
+
+#🖼️ Multiframe
 im.multiframe(1,3)
 plot(ndvi[[1]])
 plot(ndvi[[2]])
 plot(ndvi[[1]], ndvi[[2]], xlim=c(-0.3, 0.9), ylim=c(-0.3, 0.9))
 abline(0, 1, col="red")
-
