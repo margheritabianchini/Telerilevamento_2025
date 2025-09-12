8ae50973d224cfb505124ce205558fa747442a71
margheritabianchini <margherita.bianchini@hotmail.it>
2025-09-12 18:15:20 +0200

    Update 08_spatial_variability.R

--- Code/08_spatial_variability.R
+++ Code/08_spatial_variability.R
@@ -1,6 +1,8 @@
-# R code for calculating spatial variability 
-# quanto varia il sistema
+#R CODE FOR CALCULATING SPATIAL VARIABILITY
+#how much the system varies
 
+
+#Packages 
 library(terra)
 library(imageRy)
 library(viridis)
@@ -8,88 +10,121 @@ library(patchwork)
 install.packages("RStoolbox")
 library(RStoolbox)
 
-# 24 26 25 
-# ci riferiamo al calcolo della deviazione standard sulla popolazione 
-# VARIANZA = somma degli scarti quadratici diviso al numero dei campioni presi 
-# DEVIAZIONE STANDARD = radice quadrata della varianza
 
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------
+# -> MEAN = sum of the sample values ​​divided by their number
+# -> MEDIAN = indicates the central value of the sorted samples
+# -> VARIANCE = sum of the quadratic deviations divided by the number of samples taken 
+# -> STANDARD DEVIATION of the population = square root of variance
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#EXAMPLE
+#24 26 25 
+
+#Avarage
 media = (24+25+26) / 3
+#25
 
-#CALCOLO DELLA DEVIAZIONE STANDARD
-num = (24-media)^2 + (26-media)^2 + (25-media)^2
-den = 3 
-varianza = num/den 
-stdev = sqrt(varianza)
-# 0.8164966
+#Variance and standard deviation 
+num = (24-media)^2 + (26-media)^2 + (25-media)^2     #sum of the quadratic deviations
+den = 3     #number of samples taken
+variance = num/den    #variance
+#0.6666667
+stdev = sqrt(variance)     #standard deviation 
+#0.8164966
 
-sd(c(24,26,25))
-# R automaticamente calcola la dev standard sul campione (n-1)
-# con molti campioni non cambierebbe più di tanto
-# 1 
 
-sd(c(24,26,25,49))
-# ha tirato la curva ad espandersi verso una parte 
+#Standard deviation variation (automatic)
+#Three samples 
+sd(c(24,26,25))    #[sd=automatic standard deviation on the sample (n-1)]
+                   #with many samples it wouldn't change much
+#1
 
+#Four samples
+sd(c(24,26,25,49))     #extending the curve towards one side
+#12.02775
+
+
+#Mean variation (automatic) 
+#Three samples 
 mean(c(24,26,25))
+#25
+
+#Four samples 
 mean(c(24,26,25,49))
+#31
 
-# la mediana è il valore centrale. es. nel primo caso è 25
 
-#---------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+
+#Content of the package 
 im.list()
+
+
+#🛰️SENTINEL-2 BANDS (spatial resolution = 10m)
 sent = im.import("sentinel.png")
 sent=flip(sent)
 
-# Exercise: plot the image in RGB with the NIR ontop of the red component
-# in questo modo mettiamo il NIR sulla componente RED 
+
+#Layer (band)
+#1=NIR 
+
+
+#EXERCISE: plot the image in RGB with the NIR ontop of the red component
 im.plotRGB(sent, r=1, g=2, b=3)
 
-# Exercise: make three plot with NIR ontop of each component:r, g, b
-# dove si mette 3 e 2 non cambia nulla
-# è solo il vicino infrarosso che fa cambiare 
+
+#EXERCISE: make three plot with NIR ontop of each component:r, g, b
+#it doesn't change where you put 2 or 3
+#it only depends on the NIR
+#🖼️ Multiframe 
 dev.off()
 im.multiframe(1,3)
 im.plotRGB(sent, r=1, g=2, b=3)
 im.plotRGB(sent, r=2, g=1, b=3)
 im.plotRGB(sent, r=3, g=2, b=1)
-
-# verde fluorescente sono le praterie in alta quota 
-# il bianco è neve
-# oppure possono essere delle nuvole 
+#fluorescent green are the high altitude prairies
+#white is snow or clouds 
 
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-#-----------------
 
 nir = sent[[1]]
 
-# Exercise: plot the NIR band with the inferno 
 
+#EXERCISE: plot the NIR band with the inferno 
 plot(nir, col=inferno(100))
 
-sd3 = focal(nir, w=c(3,3), fun=sd)
-plot(sd3)
-# vediamo la deviazione standard su una matrice 3 x 3 
-# si vedono le zone in ciu c'è stata una variazione dell'infrarosso vicino
-# variazione in crepacci, vegetazione 
 
+#Standard deviation on a 3x3 matrix 
+sd3 = focal(nir, w=c(3,3), fun=sd)     #[focal=work pixel by pixel]
+                                       #[w=window size]
+                                       #[fun=function to apply]
+plot(sd3)
+#areas where there has been a change in the NIR
+#variation in crevasses, vegetation
+
+
+#🖼️ Multiframe 
 im.multiframe(1,2)
 im.plotRGB(sent, r=1, g=2, b=3)
 plot(sd3)
 
-# calcoliamo la deviazione standard su una finestra 5x5 
-# per variazioni di dettaglio userò una moving window ristretta
 
+#Standard deviation on a 5X5 matrix 
+#For detail variations I will use a smaller moving window
 dev.off()
 sd5 = focal(nir, w=c(5,5), fun=sd)
 plot(sd5)
 
 
+#🖼️ Multiframe 
 im.multiframe(1,2)
 plot(sd3)
 plot(sd5)
-
 
 
 # Exercise: use ggplot to plot the standard deviation
