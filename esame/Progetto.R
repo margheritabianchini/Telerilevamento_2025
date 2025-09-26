5b601c542420a4bdf16f523bd0cf3650ab77f47a
margheritabianchini <margherita.bianchini@hotmail.it>
2025-09-26 11:36:18 +0200

    Create Progetto.R

---
+++ esame/Progetto.R
@@ -0,0 +1,11 @@
+#PROGETTO TELERILEVAMENTO 2025 
+#FRANA DI PIZZO CENGALO (VAL BONDASCA)
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#🛰️Importazione delle immagini satellitari della missione SENTINEL-2 
+#  (risoluzione spaziale = 10 m)
+#  Le immagini sono state scaricate da GEE (Google Earth Engine)
+
+#Set della working directory 
+setwd("C://Users/margh/Desktop/GEOLOGIA E TERRITORIO (LM-74)/TELERILEVAMENTO/materiale progetto")
+getwd()
+pizzo_cengalo = rast(pizzo_cengalo_2017_08.tif)
