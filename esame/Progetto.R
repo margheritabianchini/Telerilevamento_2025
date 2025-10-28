842b750131fb754f097bf9f09cdc5b1d5251e422
margheritabianchini <margherita.bianchini@hotmail.it>
2025-10-28 11:43:41 +0100

    Update Progetto.R

--- esame/Progetto.R
+++ esame/Progetto.R
@@ -34,6 +34,9 @@ setwd("C://Users/margh/Desktop/GEOLOGIA E TERRITORIO (LM-74)/TELERILEVAMENTO/mat
 getwd()
 
 # Richiamo dei file 
+#Landsat 5 (evento 27.12.2011)
+pizzo_cengalo_2011 = rast("landsat5_2011.tif")
+#Sentinel-2 (evento 23.08.2017) 
 pizzo_cengalo_primavera2017 = rast("sentinel2_primavera2017.tif")
 pizzo_cengalo_pre2017 = rast("sentinel2_pre_pre_evento.tif")
 pizzo_cengalo_2017 = rast("sentinel2_estate2017.tif")
@@ -58,6 +61,9 @@ pizzo_cengalo_2018
 # 4 = NIR (b8)
 
 # Plot delle due immagini 
+# Landsat 5
+plot(pizzo_cengalo_2011)
+# Sentinel-2
 plot(pizzo_cengalo_primavera2017)
 plot(pizzo_cengalo_pre2017)
 plot(pizzo_cengalo_2017)
@@ -66,6 +72,9 @@ plot(pizzo_cengalo_2018)
 # Visualizzazione in colori naturali
 # Corrisponde alla visualizzazione nel visibile
 # [im.plotRGB = combina le tre bande in una immagine RGB]
+#Landsat 5 
+im.plotRGB(pizzo_cengalo_2011, r=1, g=2, b=3)
+#Sentine-2
 im.plotRGB(pizzo_cengalo_primavera2017, r=1, g=2, b=3)
 im.plotRGB(pizzo_cengalo_pre2017, r=1, g=2, b=3)
 im.plotRGB(pizzo_cengalo_2017, r=1, g=2, b=3)
